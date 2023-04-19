c97bd0e5132bcd55fbb4fe2a49b755ef6d565538
DanielChomon <a20180805@pucp.edu.pe>
2023-04-18 20:30:58 -0500

    Initial commit

---
+++ src/main/java/com/example/lab4_20202330/Lab420202330Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20202330;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420202330Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab420202330Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab4_20202330/Lab420202330ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20202330;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab420202330ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
